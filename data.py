0b7e30c7c2242dbd5e72514c833f53c57a450333
Sarai <anna.sarai.rosenberg@gmail.com>
2018-04-03 10:25:44 -0700

    add users

---
+++ data.py
@@ -0,0 +1,9 @@
+users = [{"id": 1,
+          "name": "Alex",
+          "key": "abc"},
+         {"id": 2,
+          "name": "Kelly",
+          "key": "klm"},
+         {"id": 3,
+          "name": "Sam",
+          "key": "stu"}]
